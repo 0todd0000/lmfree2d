8b962d2966754d62863e36ded73bb5bcbd5ff4ef
Todd Pataky <todd@h74.50.242.10.103784.vlan.kuins.net>
2020-08-24 12:47:49 +0900

    Add correspondence analysis

--- Python/shapes_4_correspondence.py
+++ Python/shapes_4_correspondence.py
@@ -12,24 +12,25 @@ from geomdl import fitting
 
 
 
-# def corresp_roll(r0, r1):
-# 	np.roll(self.verts, n, axis=0)
-#
-# 	f       = []
-# 	s       = self.copy()
-# 	for i in range(self.nvert):
-# 		s.roll_vertices(1)
-# 		f.append( s.sse(template) )
-# 	i       = np.argmin(f) + 1
-# 	s.roll_vertices(i)
-# 	return s,i,f
+def corresp_roll(r0, r1):
+	f       = []
+	n       = r1.shape[0]
+	for i in range(n):
+		r   = np.roll(r1, i, axis=0)
+		f.append( sse(r0, r) )
+	i       = np.argmin(f)
+	return np.roll(r1, i, axis=0)
 
 
 def set_npoints(r, n):
 	pcurve    = fitting.interpolate_curve(list(r), 3)
 	pcurve.sample_size = n
 	return np.asarray( pcurve.evalpts )
-	
+
+
+def sse(r0, r1):
+	return (np.linalg.norm(r1-r0, axis=1)**2).sum()
+
 
 def write_csv(fname, r):
 	shape  = np.hstack([[i+1]*rr.shape[0]  for i,rr in enumerate(r)])
@@ -41,71 +42,70 @@ def write_csv(fname, r):
 
 
 
-#(0) Find optimum-order correspondence between two shapes:
-dirREPO   = unipath.Path( os.path.dirname(__file__) ).parent
-name      = 'Comma'
-fname0    = os.path.join(dirREPO, 'Data', name, 'geom_sro.csv')
-a         = np.loadtxt(fname0, delimiter=',', skiprows=1)
-shape     = np.asarray(a[:,0], dtype=int)
-xy        = a[:,1:]
-r0        = xy[shape==1]
-r1        = xy[shape==4]
-n         = max(r0.shape[0], r1.shape[0])
-r0        = set_npoints(r0, n)
-r1        = set_npoints(r1, n)
-### check ordering:
-plt.close('all')
-fig,AX  = plt.subplots( 1, 2, figsize=(8,3) )
-ax0,ax1 = AX.flatten()
-ax0.scatter(r0[:,0], r0[:,1], c=np.arange(n), cmap='jet', vmin=0, vmax=n)
-ax1.scatter(r1[:,0], r1[:,1], c=np.arange(n), cmap='jet', vmin=0, vmax=n)
-# ax1.fill(r1[:,0], r1[:,1], fill=False)
-ax0.axis('equal')
-ax1.axis('equal')
-plt.show()
-
-
-
-
-
-# #(1) Order points for one dataset:
+# #(0) Find optimum-order correspondence between two shapes:
 # dirREPO   = unipath.Path( os.path.dirname(__file__) ).parent
-# names     = ['Bell', 'Comma', 'Device8',    'Face', 'Flatfish', 'Hammer',    'Heart', 'Horseshoe', 'Key']
-# name      = names[5]
-# fname0    = os.path.join(dirREPO, 'Data', name, 'geom_sr.csv')
+# name      = 'Bell'
+# fname0    = os.path.join(dirREPO, 'Data', name, 'geom_sro.csv')
+# a         = np.loadtxt(fname0, delimiter=',', skiprows=1)
+# shape     = np.asarray(a[:,0], dtype=int)
+# xy        = a[:,1:]
+# r0        = xy[shape==1]
+# r1        = xy[shape==2]
+# n         = max(r0.shape[0], r1.shape[0])
+# r0        = set_npoints(r0, n)
+# r1        = set_npoints(r1, n)
+# r2        = corresp_roll(r0, r1)
+# ### check ordering:
+# plt.close('all')
+# fig,AX  = plt.subplots( 1, 3, figsize=(12,3) )
+# ax0,ax1,ax2 = AX.flatten()
+# ax0.scatter(r0[:,0], r0[:,1], c=np.arange(n), cmap='jet', vmin=0, vmax=n)
+# ax1.scatter(r1[:,0], r1[:,1], c=np.arange(n), cmap='jet', vmin=0, vmax=n)
+# ax2.scatter(r2[:,0], r2[:,1], c=np.arange(n), cmap='jet', vmin=0, vmax=n)
+# [ax.axis('equal') for ax in AX]
+# plt.show()
+
+
+# #(1) Find optimum-order correspondence (one dataset):
+# dirREPO   = unipath.Path( os.path.dirname(__file__) ).parent
+# name      = 'Bell'
+# fname0    = os.path.join(dirREPO, 'Data', name, 'geom_sro.csv')
 # a         = np.loadtxt(fname0, delimiter=',', skiprows=1)
 # shape     = np.asarray(a[:,0], dtype=int)
 # xy        = a[:,1:]
 # r0        = [xy[shape==u]  for u in np.unique(shape)]
-# r1        = [reorder_points(rr, optimum_order=True)  for rr in r0]
-# r1        = [order_points_clockwise(rr) for rr in r1]
+# nn        = [rr.shape[0]  for rr in r0]
+# n         = max( nn )
+# rtemp     = r0[ np.argmax(nn) ]
+# r1        = [set_npoints(rr, n) for rr in r0]
+# r1        = [corresp_roll(rtemp, rr)  for rr in r1]
 # ### check ordering:
 # plt.close('all')
-# fig,AX  = plt.subplots( 1, 2, figsize=(8,3) )
-# ax0,ax1 = AX.flatten()
-# [ax0.fill(rr[:,0], rr[:,1], fill=False, edgecolor='0.7')  for rr in r0]
-# [ax1.fill(rr[:,0], rr[:,1], fill=False, edgecolor='0.7')  for rr in r1]
-# ax0.axis('equal')
-# ax1.axis('equal')
+# fig,AX  = plt.subplots( 2, 5, figsize=(12,6) )
+# for ax,rr in zip(AX.flatten(), r1):
+# 	ax.scatter(rr[:,0], rr[:,1], c=np.arange(rr.shape[0]), cmap='jet', vmin=0, vmax=n)
+# 	ax.axis('equal')
 # plt.show()
 
 
 
+#(2) Process all datasets:
+dirREPO   = unipath.Path( os.path.dirname(__file__) ).parent
+names     = ['Bell', 'Comma', 'Device8',    'Face', 'Flatfish', 'Hammer',    'Heart', 'Horseshoe', 'Key']
+for name in names:
+	print( f'Finding roll correspondence for {name} dataset...' )
+	fname0    = os.path.join(dirREPO, 'Data', name, 'geom_sro.csv')
+	fname1    = os.path.join(dirREPO, 'Data', name, 'geom_sroc.csv')
+	a         = np.loadtxt(fname0, delimiter=',', skiprows=1)
+	shape     = np.asarray(a[:,0], dtype=int)
+	xy        = a[:,1:]
+	r0        = [xy[shape==u]  for u in np.unique(shape)]
+	nn        = [rr.shape[0]  for rr in r0]
+	n         = max( nn )
+	rtemp     = r0[ np.argmax(nn) ]
+	r1        = [set_npoints(rr, n) for rr in r0]
+	r1        = [corresp_roll(rtemp, rr)  for rr in r1]
+	write_csv(fname1, r1)
 
-# #(2) Order points for all datasets:
-# dirREPO   = unipath.Path( os.path.dirname(__file__) ).parent
-# names     = ['Bell', 'Comma', 'Device8',    'Face', 'Flatfish', 'Hammer',    'Heart', 'Horseshoe', 'Key']
-# for name in names:
-# 	print( f'Ordering points for {name} dataset...' )
-# 	fname0    = os.path.join(dirREPO, 'Data', name, 'geom_sr.csv')
-# 	fname1    = os.path.join(dirREPO, 'Data', name, 'geom_sro.csv')
-# 	a         = np.loadtxt(fname0, delimiter=',', skiprows=1)
-# 	shape     = np.asarray(a[:,0], dtype=int)
-# 	xy        = a[:,1:]
-# 	for u in np.unique(shape):
-# 		i     = shape==u
-# 		r     = [xy[shape==u]  for u in np.unique(shape)]
-# 		r     = [reorder_points(rr, optimum_order=True)  for rr in r]
-# 		r     = [order_points_clockwise(rr) for rr in r]
-# 		write_csv(fname1, r)
+
 
