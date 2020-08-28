bdd88ffa937b9a75e3a6cd5f6c92dd1981412a55
Todd Pataky <todd@h74.50.242.10.103784.vlan.kuins.net>
2020-08-28 15:32:47 +0900

    Update table_npoints.py

--- Python/table_npoints.py
+++ Python/table_npoints.py
@@ -1,22 +1,21 @@
 
-import os,unipath
+import os
 import numpy as np
+import lmfree2d as lm
 
 
-
-#(0) Load data:
-dirREPO    = unipath.Path( os.path.dirname(__file__) ).parent
+#(0) Load data and summarize the number of contour points in each dataset:
+dirREPO    = lm.get_repository_path()
 names      = ['Bell', 'Comma', 'Device8', 'Face',    'Flatfish', 'Hammer', 'Heart', 'Horseshoe', 'Key']
 npoints    = []
 for name in names:
 	fname0 = os.path.join(dirREPO, 'Data', name, 'contours.csv')
-	a      = np.loadtxt(fname0, delimiter=',', skiprows=1)
-	shape  = np.asarray(a[:,0], dtype=int)
-	n      = np.asarray(  [(shape==u).sum()  for u in np.unique(shape)],  dtype=int)
+	r      = lm.read_csv(fname0)
+	n      = np.asarray( [rr.shape[0]  for rr in r] )
 	npoints.append( [n.min(), int( np.median(n) ), n.max()] )
 
 
-#(1) Write results:
+#(1) Write the results:
 fname1 = os.path.join(dirREPO, 'Results', 'table_npoints.csv')
 with open(fname1, 'w') as f:
 	f.write('Name,Min,Median,Max\n')
