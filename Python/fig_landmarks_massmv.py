02e9f3b6d8fc930c464531f573fae83e12b69ad1
Todd Pataky <todd@h74.50.242.10.103784.vlan.kuins.net>
2020-08-28 16:27:43 +0900

    Update landmarks figure

--- Python/fig_landmarks_massmv.py
+++ Python/fig_landmarks_massmv.py
@@ -7,25 +7,9 @@ import os,unipath
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+import lmfree2d as lm
 
 
-plt.rcParams['mathtext.fontset'] = 'stix'
-plt.rcParams['font.family']      = 'Arial'
-plt.rcParams['xtick.labelsize']  = 8
-plt.rcParams['ytick.labelsize']  = 8
-
-
-colors = np.array([
-	[177,139,187],
-	[166,154,196],
-	[132,118,181],
-	[225,215,231],
-	[252,227,205],
-	[231,179,159],
-	[213,160,104],
-	[166,198,226],
-	[134,167,202],
-   ]) / 255
 
 
 #(0) Load results:
@@ -37,6 +21,7 @@ ndatasets = len(df)
 
 #(1) Plot:
 plt.close('all')
+lm.set_matplotlib_rcparams()
 plt.figure( figsize=(10,7) )
 axx       = np.linspace(0.05, 0.70, 3)
 axy       = np.linspace(0.71, 0.06, 3)
@@ -48,10 +33,10 @@ for i,ax in enumerate(AX.flatten()):
 	y     = np.array(row.values[4:], dtype=float)
 	y     = y[np.logical_not(np.isnan(y))]
 	x     = 1 + np.arange(y.size)
-	ax.bar(x, y, color=colors[1])
+	ax.bar(x, y, color=lm.colors[1])
 	zc    = row['T2crit']
-	ax.axhline(zc, color=colors[0], ls='--' )
-	ax.text(5.7, zc+1.5, r'$T^2_{critical} = %.3f$'%zc, size=10, color=colors[0])
+	ax.axhline(zc, color=lm.colors[0], ls='--' )
+	ax.text(5.7, zc+1.5, r'$T^2_{critical} = %.3f$'%zc, size=10, color=lm.colors[0])
 	ax.text(1, 30, r'$p = %.3f$'%row['P'], size=10)
 	ax.set_title( row['Name'], color='k', size=16 )
 [ax.set_xlabel('Landmark', color='k', size=14)  for ax in AX[2]]
